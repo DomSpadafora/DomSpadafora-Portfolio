b22a53138a90fa59d2c195c5f8fe40138c997705
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-02 15:39:17 -0500

    header commit

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Resume</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <h1>I am Dom Spadafora</h1>
+    </header>
+  )
+}
+
+export default Header
+
