9d0c6ec26463e07d1520caf215ea7a62be8691c9
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-06 13:36:49 -0500

    added router to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import Navigation from './Navigation';
+import AboutMe from './AboutMe';
+import Portfolio from './Portfolio'
+import Contact from './Contact';
+import Resume from './Resume';
+import Home from './Home';
 
-const Nav = () => {
-  const linkStyle = { border: '1px black', padding: '5px' };
+class Header extends Component {
+  render() {
+    return (
+    <Router>
 
-  return (
-    <nav className="main-header-menu">
-      <section
-        style={{
-          display: 'flex',
-          fontFamily: 'helvetica',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <div style={linkStyle}>
-          <a href="home">Home</a>
+        <div>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about-me'} className="nav-link">About</Link></li>
+            <li><Link to={'/resume'} className="nav-link">Resume</Link></li>
+            <li><Link to={'/portfolio'} className="nav-link">Porfolio</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Routes>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about-me' component={AboutMe} />
+              <Route path='/resume' component={Resume} />
+              <Route path='/portfolio' component={Portfolio} />
+          </Routes>
         </div>
-        <div style={linkStyle}>
-          <a href="about-me">About Me</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="resume">Resume</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="projects">Projects</a>
-        </div>
-        <div style={linkStyle}>
-          <a href="contact">Contact</a>
-        </div>
-      </section>
-    </nav>
-  );
+      </Router>
+    );
+  }
 }
-
-export default Nav
+export default Header
