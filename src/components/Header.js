07bf675abd7e94b72921d36a9ba6a9b441d42357
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-07 18:00:18 -0500

    background image and home.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,19 +17,19 @@ const Header = () =>
         
         <ul className="navbar-nav navbar-collapse ">
           <li><Link to={'/'} className= 'nav-link' > Home </Link></li>
-          <li><Link to={'/contact'} className= 'nav-link'>Contact</Link></li>
           <li><Link to={'/about-me'} className= 'nav-link'>About</Link></li>
           <li><Link to={'/resume'} className= 'nav-link'>Resume</Link></li>
           <li><Link to={'/portfolio'} className= 'nav-link'>Porfolio</Link></li>
+          <li><Link to={'/contact'} className= 'nav-link'>Contact</Link></li>
         </ul>
       </nav>
       <hr />
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
         <Route path='/about-me' element={<AboutMe />} />
         <Route path='/resume' element={<Resume />} />
         <Route path='/portfolio' element={<Portfolio />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import '../styles/Home.css'
 
 const Home = () => {
   return (
-      
-      <div className="row banner">
+    <body>
+      <card className="row banner">
         <h1>Dom Spadafora</h1>
         <p>
           Currently a Full-Stack Web Developer aspiring to create the best opportunities for my family! Take a look around to learn more about me.
         </p>
-      </div>
-
+      </card>
+    </body>
 
   )
 }
