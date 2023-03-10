4c531cedb75b58f6922870d8a4344edc378b38f3
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-09 22:14:18 -0500

    gh pages

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-// React apps typically have a single App component at the very top that can reference other React components.
-// This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
+
 function App() {
   return (
 
