14db7c987d48b6b6be284f365450f94b83bd1689
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-09 21:56:15 -0500

    add home to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home from './components/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +12,7 @@ function App() {
 
     <div>
       <Header />
+      <Home />
       <Footer />
     </div>
 
