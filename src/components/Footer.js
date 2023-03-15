68856cf6227bf42355545abb7affca878abb8405
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-15 15:59:49 -0400

    styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../styles/Footer.css';
 const Footer = () => {
     return (
         <div className='footer'>
-            <footer className="footer fixed-bottom p-3">
+            <footer className="footer fixed-bottom p-3 mb-4">
                 <a href="https://github.com/domspadafora"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon" /></a>
                 <a href="https://www.linkedin.com/in/dom-spadafora-81b76480/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon" /></a>
             </footer>
