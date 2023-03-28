b42fbcc5e64f7bdd6bc6fe3fceb05c5582521fe4
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-03-27 20:54:54 -0400

    embed

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,7 +2,7 @@ import React from 'react'
 import samplePDF from '../Resume/resume.pdf'
 import '../../styles/Resume.css'
 
-
+const samplePDF = 'https://docs.google.com/document/d/1GknG1WKRqvlZKbxfbxfDp01aK2AIzZpdjmPKo3Esg9k/edit?usp=sharing'
 
 const Resume = () => {
   return (
