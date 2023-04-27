9c034be0f74cfcd875c1bf9141644cc23cdae51d
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-04-27 12:45:27 -0400

    change description on home pg

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="banner">
         <h1>Dom Spadafora</h1>
         <p>
-          Currently a Full-Stack Web Developer aspiring to create the best opportunities for my family! Take a look around to learn more about me.
+          A Full-Stack Web Developer, curious to learn just about anything! Take a look around to learn more about me.
         </p>
       </div>
     </div>
